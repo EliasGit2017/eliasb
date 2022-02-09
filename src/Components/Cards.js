8674c5ef5ac503d4da90e14e986cd82d14367fbb
Elias2049 <azwbdj@gmail.com>
2022-02-09 01:02:05 +0100

    adds itw

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -11,33 +11,33 @@ function Cards() {
                     <ul className="cards__items">
                         <CardItem 
                             src={process.env.PUBLIC_URL+"/images/lol.jpg"}
-                            text="Check out the interviews made by Damien"
+                            text="Check out the interview made by Fares"
                             label="Interviews"
                             gotothis="https://github.com/DamienLegros/IHMPROJECT/tree/main/Interviews%20Damien"
                         />
                         <CardItem 
                             src={process.env.PUBLIC_URL+"/images/img-11.jpg"} 
-                            text="Check out the interviews made by Lydia"
+                            text="Check out the interview made by Lyna"
                             label="Interviews" 
-                            gotothis="https://github.com/DamienLegros/IHMPROJECT/blob/main/Interviews%20Lydia/IntrvwsLydia.pdf"
+                            gotothis=""
                         />
                         <CardItem 
                             src={process.env.PUBLIC_URL+"/images/img-12.jpg"}
-                            text="Check out the interviews made by Elias"
+                            text="Check out the interview made by Elias"
                             label="Interviews" 
-                            gotothis="https://github.com/DamienLegros/IHMPROJECT/tree/main/Interviews%20Elias"
+                            gotothis="https://drive.google.com/file/d/1-SQugxs4AMUeERDB3Ck0uqccsqMveB_d/view?usp=sharing"
                         />
                         <CardItem 
                             src={process.env.PUBLIC_URL+"/images/img-13.jpg"}
-                            text="Check out the interviews made by Sarah"
+                            text="Check out the interview made by David"
                             label="Interviews" 
-                            gotothis="https://github.com/DamienLegros/IHMPROJECT/blob/main/Interviews%20Sarah/interview%20sarah%20-%20R%C3%A9ponses%20au%20formulaire%201.pdf"
+                            gotothis="https://drive.google.com/file/d/1UVL-AVT88ghu7F3p4kleHaPQg-xzQf3w/view?usp=sharing"
                         />
                         <CardItem 
                             src={process.env.PUBLIC_URL+"/images/lost_dav.jpg"}
-                            text="Check out the interviews made by David"
+                            text="Check out the interview made by Lorenzo"
                             label="Interviews" 
-                            gotothis="https://github.com/DamienLegros/IHMPROJECT/blob/main/Interviews%20David/IHM-Interview.pdf"
+                            gotothis="https://drive.google.com/file/d/1gnyQHfrRKaAbh2w-a1LsuyhRkP0R8ruk/view?usp=sharing"
                         />
                     </ul>
                 </div>
