e32adf34aa52fa8c5701b3393310e8f0fbb9e55e
EliasGit2017 <azouaou.bendjaballah@etu.upmc.fr>
2022-05-21 12:58:11 +0200

    fixing 2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import SignUp from './Components/pages/SignUp'
 function App() {
   return (
     <>
-      <Router basename="/ihm-cooking-project">
+      <Router basename="/elias-b">
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
