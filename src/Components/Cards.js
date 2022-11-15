a98b6715b656e9cf9585049c3865ba1ce75c7705
EliasGit2017 <azouaou.bendjaballah@etu.upmc.fr>
2022-11-15 11:14:47 +0100

    add ressources

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -9,25 +9,36 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem 
-                            src={process.env.PUBLIC_URL+"/images/s1_docs.png"}
+                        <CardItem
+                            src={process.env.PUBLIC_URL + "/images/s1_docs.png"}
                             text="Digodoc : an Ocaml documentation explorer"
                             label="Ocaml"
                             gotothis="https://github.com/EliasGit2017/digodoc"
                         />
-                        <CardItem 
-                            src={process.env.PUBLIC_URL+"/images/g_opt.png"} 
+                        <CardItem
+                            src={process.env.PUBLIC_URL + "/images/g_opt.png"}
                             text="Operations Research : Modelization & Optimization applied to health models"
-                            label="Python" 
+                            label="Python"
                             gotothis="https://github.com/EliasGit2017/Optimisation-localisation-unites-de-soins-et-prise-en-charge"
                         />
-                        <CardItem 
-                            src={process.env.PUBLIC_URL+"/images/sud.png"}
+                        <CardItem
+                            src={process.env.PUBLIC_URL + "/images/serv_arch.png"}
+                            text="SMT & Theorem provers server (designed as a StraExec clone)"
+                            label="OCaml"
+                            gotothis="https://github.com/EliasGit2017/ProofBox.git"
+                        />
+                        <CardItem
+                            src={process.env.PUBLIC_URL + "/images/dockerhub.png"}
+                            text="Docker images of the alt-ergo SMT solver"
+                            label="OCaml"
+                            gotothis="https://github.com/EliasGit2017/alt-ergo-docker-images.git"
+                        />
+                        <CardItem
+                            src={process.env.PUBLIC_URL + "/images/sud.png"}
                             text="Sudoku Game with integrated SAT solver. In depth exploration of Clojure"
-                            label="Clojure" 
+                            label="Clojure"
                             gotothis="https://github.com/EliasGit2017/Clojure_Sudoku_Game-Solver.git"
                         />
-
                         {/* Improve this by adding new elements when available */}
 
                         {/* <CardItem 
